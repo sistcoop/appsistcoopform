f29787f75ec2746c440edb38cd7acf09452947f4
Carlos Feria <carlosthe19916@gmail.com>
2016-03-03 16:56:57 -0500

    select answer directives

--- app/scripts/directives/answer-select/answer.select.multiple.directive.js
+++ app/scripts/directives/answer-select/answer.select.multiple.directive.js
@@ -17,19 +17,54 @@ angular.module('forms').directive('scAnswerSelectMultiple', function () {
 
     }],
     link: function (scope, element, attrs, ngModel) {
+
+      function intersect(a, b) {
+        var t;
+        if (b.length > a.length) t = b, b = a, a = t; // indexOf to loop over shorter
+        return a.filter(function (e) {
+          if (b.indexOf(e) !== -1) return true;
+        });
+      }
+
+      var arr_diff = function  (a1, a2) {
+
+        var a = [], diff = [];
+
+        for (var i = 0; i < a1.length; i++) {
+          a[a1[i]] = true;
+        }
+
+        for (var i = 0; i < a2.length; i++) {
+          if (a[a2[i]]) {
+            delete a[a2[i]];
+          } else {
+            a[a2[i]] = true;
+          }
+        }
+
+        for (var k in a) {
+          diff.push(k);
+        }
+
+        return diff;
+      };
+
       scope.$watch(function () {
         return ngModel.$modelValue;
       }, function (newValue, oldValue) {
         if(newValue) {
+          var options = [];
           for(var i = 0; i < scope.options.length; i++) {
-            if(scope.options[i].denomination === newValue[0]) {
-              if(scope.options[i].editable) {
-                scope.selectedEditable = newValue[0]; break;
-              } else {
-                scope.selectedNotEditable = newValue[0]; break;
-              }
-            }
+            options.push(scope.options[i].denomination);
           }
+
+          scope.selectedNotEditable = intersect(options, newValue)[0];
+
+          var dif = arr_diff(newValue, intersect(options, newValue));
+          if(dif.length) {
+            scope.selectedEditable = dif[0];
+          }
+
         }
       }, true);
 
--- app/scripts/directives/answer-select/answer.select.verification.directive.js
+++ app/scripts/directives/answer-select/answer.select.verification.directive.js
@@ -17,11 +17,56 @@ angular.module('forms').directive('scAnswerSelectVerification', function () {
 
     }],
     link: function (scope, element, attrs, ngModel) {
+      scope.selectedEditable = [];
+      scope.selectedNotEditable = [];
+
+      function intersect(a, b) {
+        var t;
+        if (b.length > a.length) t = b, b = a, a = t; // indexOf to loop over shorter
+        return a.filter(function (e) {
+          if (b.indexOf(e) !== -1) return true;
+        });
+      }
+
+      var arr_diff = function  (a1, a2) {
+
+        var a = [], diff = [];
+
+        for (var i = 0; i < a1.length; i++) {
+          a[a1[i]] = true;
+        }
+
+        for (var i = 0; i < a2.length; i++) {
+          if (a[a2[i]]) {
+            delete a[a2[i]];
+          } else {
+            a[a2[i]] = true;
+          }
+        }
+
+        for (var k in a) {
+          diff.push(k);
+        }
+
+        return diff;
+      };
+
       scope.$watch(function () {
         return ngModel.$modelValue;
       }, function (newValue, oldValue) {
         if(newValue) {
-          scope.selected = newValue;
+          var options = [];
+          for(var i = 0; i < scope.options.length; i++) {
+            options.push(scope.options[i].denomination);
+          }
+
+          scope.selectedNotEditable = intersect(options, newValue);
+
+          var dif = arr_diff(newValue, intersect(options, newValue));
+          if(dif.length) {
+            scope.selectedEditable = dif;
+          }
+
         }
       }, true);
 
